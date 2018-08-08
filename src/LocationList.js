9deb3e723c6d0feb0c22461f92581d5d07cb97d9
Jason Arnold <jason.arnold.00@gmail.com>
2018-08-08 13:00:06 -0400

    input field is controlled

--- src/LocationList.js
+++ src/LocationList.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
 
 class LocationList extends Component {
+  state = {
+    input: ''
+  }
+
+  onInputChange = e => {
+    e.preventDefault()
+    this.setState({ input: e.target.value })
+  }
+
   render() {
     return (
-      <ul>
-        <li>Location 1</li>
-        <li>Location 2</li>
-        <li>Location 3</li>
-        <li>Location 4</li>
-        <li>Location 5</li>
-      </ul>
+      <div>
+        <input type="text" onChange={(e) => this.onInputChange(e)} />
+        <ul>
+          <li>Location 1</li>
+          <li>Location 2</li>
+          <li>Location 3</li>
+          <li>Location 4</li>
+          <li>Location 5</li>
+        </ul>
+      </div>
     )
   }
 }
