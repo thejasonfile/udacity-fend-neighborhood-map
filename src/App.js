953b9b1923233589b3123ac24db9a383f1740e1e
Jason Arnold <jason.arnold.00@gmail.com>
2018-08-08 12:54:05 -0400

    add LocationList component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Title from './Title'
+import LocationList from './LocationList'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Title />
+        <LocationList />
       </div>
     );
   }
---
+++ src/LocationList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class LocationList extends Component {
+  render() {
+    return (
+      <ul>
+        <li>Location 1</li>
+        <li>Location 2</li>
+        <li>Location 3</li>
+        <li>Location 4</li>
+        <li>Location 5</li>
+      </ul>
+    )
+  }
+}
+
+export default LocationList
