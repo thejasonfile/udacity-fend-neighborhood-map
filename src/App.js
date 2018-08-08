d5d3411e2dc93ae208d62ddd5fe8f886860d644f
Jason Arnold <jason.arnold.00@gmail.com>
2018-08-08 12:51:45 -0400

    added Title component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Title from './Title'
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello!
+        <Title />
       </div>
     );
   }
---
+++ src/Title.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Title = () => {
+  return (
+    <h1>Points of Interest in Lower Manhattan</h1>
+  )
+}
+
+export default Title
