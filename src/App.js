44a2c59f621bd1ec7248e1a0959ecbc9491c3a47
Jason Arnold <jason.arnold.00@gmail.com>
2018-08-08 12:56:01 -0400

    add GoogleMap component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Title from './Title'
 import LocationList from './LocationList'
+import GoogleMap from './GoogleMap'
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div>
         <Title />
         <LocationList />
+        <GoogleMap />
       </div>
     );
   }
---
+++ src/GoogleMap.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class GoogleMap extends Component {
+  render() {
+    return (
+      <div>Google Map</div>
+    )
+  }
+}
+
+export default GoogleMap
