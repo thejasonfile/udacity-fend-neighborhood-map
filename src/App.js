4558fc0f6856c7a1d877cccbf0b9b94241657ea5
Jason Arnold <jason.arnold.00@gmail.com>
2018-08-18 11:55:41 -0400

    render basic map

--- src/App.js
+++ src/App.js
@@ -1,78 +1,41 @@
 import React, { Component } from 'react';
-import Title from './Title'
-import LocationList from './LocationList'
-import GoogleMap from './GoogleMap'
 
 class App extends Component {
-  state = {
-    locations: [
-      {
-        title: 'Statue of Liberty',
-        lat: 40.6892,
-        lng: -74.0445
-      },
-      {
-        title: 'Ellis Island',
-        lat: 40.6995,
-        lng: -74.0396
-      },
-      {
-        title: 'Governor\'s Island',
-        lat: 40.6895,
-        lng: -74.0168
-      },
-      {
-        title: 'New York Stock Exchange',
-        lat: 40.7067,
-        lng: -74.0112
-      },
-      {
-        title: 'Battery Park',
-        lat: 40.7033,
-        lng: -74.0170
-      },
-      {
-        title: 'The Charging Bull',
-        lat: 40.7055,
-        lng: -74.0134
-      },
-      {
-        title: 'Federal Hall',
-        lat: 40.7074,
-        lng: -74.0102
-      },
-      {
-        title: 'One World Trade Center',
-        lat: 40.7127,
-        lng: -74.0134
-      },
-      {
-        title: '9/11 Memorial',
-        lat: 40.7116,
-        lng: -74.0133
-      }
-    ],
-    allMarkers: [],
-    currentMarker: null
+
+  componentDidMount() {
+    this.renderMap()
   }
 
-  setCurrentMarker = marker => {
-    this.setState({ currentMarker: marker })
+  renderMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyAEF0NBGLZqmzeUN1uUta4x9hBJf63Pb5g&callback=initMap")
+    window.initMap = this.initMap
+  }
+
+  initMap = () => {
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
   }
 
   render() {
     return (
-      <div>
-        <Title />
-        <LocationList
-          locations = {this.state.locations}
-        />
-        <GoogleMap
-          locations = {this.state.locations}
-        />
-      </div>
+      <main>
+        <div id="map">
+
+        </div>
+      </main>
     );
   }
+}
+
+function loadScript(url) {
+  var index = window.document.getElementsByTagName('script')[0]
+  var script = window.document.createElement('script')
+  script.src = url
+  script.async = true
+  script.defer = true
+  index.parentNode.insertBefore(script, index)
 }
 
 export default App;
--- src/Title.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Title = () => {
-  return (
-    <h1>Points of Interest in Lower Manhattan</h1>
-  )
-}
-
-export default Title
