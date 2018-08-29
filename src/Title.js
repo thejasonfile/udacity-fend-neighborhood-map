d55722a531273c9a8568b856d721a8014c09d831
Jason Arnold <jason.arnold.00@gmail.com>
2018-08-29 13:58:53 -0400

    made Title a function

--- src/Title.js
+++ src/Title.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Title extends Component {
-  render() {
-    return (
-      <header id="title">
-        <h1>Points of Interest in Lower Manhattan</h1>
-      </header>
-    )
-  }
+const Title = () => {
+  return (
+    <header id="title">
+      <h1>Points of Interest in Lower Manhattan</h1>
+    </header>
+  )
 }
 
 export default Title
